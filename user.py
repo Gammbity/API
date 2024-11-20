eaed1cc29d117f7f56829a85e19c6a15e409def1
Gammbity <abduboriyabdusamadov66@gmail.com>
2024-11-20 23:20:03 +0500

    create users own file with his email

--- user.py
+++ user.py
@@ -1,9 +1,10 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, jsonify
 import secrets
+import os
 
 app = Flask(__name__, template_folder="templates")
 
-users = {}
+dir = "users_files"
 
 @app.route("/", methods=["GET"])
 def main():
@@ -15,11 +16,15 @@ def registration():
     email = request.form.get("email")
     password = request.form.get("password")
     token = secrets.token_hex(8)
-    index = len(users)
-    users[index] = {"name": name, "email": email, "password": password, "token": token}
+    file_name = f"{email}.txt"
+    file_path = os.path.join(dir, file_name)
+    if os.path.exists(file_path):
+        return jsonify({'error': 'Foydalanuvchi allaqachon ro‘yxatdan o‘tgan!'}), 400
+    with open(file_path, 'w') as file:
+        file.write(f"Name: {name}\nEmail: {email}\nPassword: {password}\nToken: {token}")
     with open('users.csv', 'a') as file:
         file.writelines(f"{name},{email},{password},{token}\n")
-    return render_template("create_post.html", name="Create Post page")
+    return render_template("my_file.html", name="Create Post page")
 
 if __name__ == "__main__":
     app.run(debug=True, host='0.0.0.0', port='5000')
